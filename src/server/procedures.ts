a75bd39cebb7ed4321b49e9be94fa224b1e21385
unknown <aminezouguig@proton.me>
2024-11-30 13:07:42 +0100

    Setup project Npm install setup database prisma

---
+++ src/server/procedures.ts
@@ -0,0 +1,15 @@
+import { Pool } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client"
+import { Redis } from "@upstash/redis/cloudflare"
+import { env } from "hono/adapter"
+import { cacheExtension } from "./__internals/db/cache-extension"
+import { j } from "./__internals/j"
+
+/**
+ * Public (unauthenticated) procedures
+ *
+ * This is the base piece you use to build new queries and mutations on your API.
+ */
+export const baseProcedure = j.procedure
+export const publicProcedure = baseProcedure
