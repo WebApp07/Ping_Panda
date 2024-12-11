747fd510f1d63c9b9d1f9640625cb6355c527d7e
unknown <aminezouguig@proton.me>
2024-12-11 20:36:14 +0100

    feat: start writing the first router (work in progress)

--- src/server/routers/auth-router.ts
+++ src/server/routers/auth-router.ts
@@ -1,3 +1,8 @@
 import { router } from "../__internals/router"
+import { publicProcedure } from "../procedures"
 
-export const AuthRouter = router({})
+export const AuthRouter = router({
+  getDataSyncStatus: publicProcedure.query(({ c }) => {
+    return c.json({ status: "Success" })
+  }),
+})
